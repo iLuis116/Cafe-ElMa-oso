a2e7dada9fdb38f9ff7485d7bb24836c00060073
JoseLuisCasa <luisflores2103@gmail.com>
2022-06-29 18:06:06 -0500

    efecto del scroll

--- js/efectos.js
+++ js/efectos.js
@@ -21,3 +21,39 @@ $(document).ready(function(){
 			marginTop: '-52px'
 		}, 1500);
 	}
+    var acercaDe = $('#acerca-de').offset().top,
+		menu = $('#platillos').offset().top,
+		galeria = $('#galeria').offset().top,
+		ubicacion = $('#ubicacion').offset().top;
+
+
+	$('#btn-acerca-de').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: acercaDe - 100
+		}, 500);
+	});
+
+		$('#btn-menu').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: menu
+		}, 500);
+	});
+
+	$('#btn-galeria').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: galeria
+		}, 500);
+	});
+
+	$('#btn-ubicacion').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: ubicacion
+		}, 500);
+	});
+
+
+});
